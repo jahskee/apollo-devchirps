959dd3bcda3781799d9682bbd345791fe6d7ac77
jahskee <jahskee@yahoo.com>
2021-06-19 18:30:05 -0400

    fix

--- server/src/services/accounts/resolvers.js
+++ server/src/services/accounts/resolvers.js
@@ -1,9 +1,24 @@
+const accounts = [
+  {
+    id: "1",
+    email: "devchirp@mandiwise.com",
+  },
+];
+
 const resolvers = {
+  Account: {
+    __resolveReference(ref, ctx, info) {
+      return accounts.find((account) => account.id === ref.id);
+    },
+  },
   Query: {
     hello: () => {
       return "hello world";
-    }
-  }
-}
+    },
+    viewer: () => {
+      return accounts[0];
+    },
+  },
+};
 
 export default resolvers;
--- server/src/services/accounts/typeDefs.js
+++ server/src/services/accounts/typeDefs.js
@@ -8,6 +8,7 @@ const typeDefs = gql`
 
   extend type Query {
     hello: String
+    viewer: Account
   }
 `;
 
