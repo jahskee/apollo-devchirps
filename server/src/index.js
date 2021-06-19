7f95878c24956663eac17b4cd9007977b364fa5e
jahskee <jahskee@yahoo.com>
2021-06-19 18:02:18 -0400

    update

--- server/src/index.js
+++ server/src/index.js
@@ -20,5 +20,5 @@ import { ApolloServer} from "apollo-server-express";
   server.applyMiddleware({ app })
 
   await new Promise(resolve => app.listen({ port }, resolve));
-  console.log(`🚀 Server ready at ${server_url}${server.graphqlPath}`);
+  console.log(`🚀 Gateway Server ready at ${server_url}${server.graphqlPath}`);
 })();
